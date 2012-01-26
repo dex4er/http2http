baf84dd2cf147fbf0b0dc244ff5ce45f90cd3c88
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-01-26 11:16:43 +0100

    New proxies: http2ws and ws2http

---
+++ ws2http.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/env perl
+
+# WS to HTTP proxy server
+#
+# Usage:
+#   ws2http.pl [host:port]
+
+use strict;
+use warnings;
+
+use constant SERVER_PORT => 2080;
+
+use HTTP::Proxy ':log';
+use HTTP::Proxy::HeaderFilter::simple;
+use HTTP::Proxy::BodyFilter::simple;
+use HTTP::Proxy::BodyFilter::complete;
+
+use constant::boolean;
+
+
+my ($host, $port) = split /:/, $ARGV[0] if $ARGV[0];
+
+my $proxy = HTTP::Proxy->new(
+    host => $host || '127.0.0.1',
+    port => $port || SERVER_PORT,
+    logmask => PROCESS | SOCKET | STATUS | DATA,
+    via => undef,
+    x_forwarded_for => undef,
+);
+
+$proxy->push_filter(
+    mime => '*/*',
+    request => HTTP::Proxy::HeaderFilter::simple->new(
+        sub {
+            my ($self, $headers, $request) = @_;
+            my $content = $request->content;
+            $content =~ s/\n/ /sg;
+            my $uri = $request->uri;
+            $proxy->log(DATA, 'REQUEST', sprintf '%s:%s|%s %s|%s|%s',
+                $proxy->client_socket->peerhost, $proxy->client_socket->peerport,
+                $request->method, $uri->as_string, $request->headers->as_string('|'), $content);
+        }
+    ),
+    response => HTTP::Proxy::BodyFilter::complete->new,
+    response => HTTP::Proxy::BodyFilter::simple->new(
+        sub {
+            my ($self, $dataref, $response, $protocol, $buffer) = @_;
+            return if defined $buffer;
+            my $content = $$dataref;
+            $content =~ s/\n/ /sg;
+            $proxy->log(DATA, 'RESPONSE', sprintf '%s:%s|%s|%s|%s',
+                $proxy->client_socket->peerhost, $proxy->client_socket->peerport,
+                $response->status_line, $response->headers->as_string('|'), $content);
+        }
+    ),
+);
+
+$proxy->push_filter(
+    mime => '*/*',
+    request => HTTP::Proxy::HeaderFilter::simple->new(
+        sub {
+            my ($self, $headers, $request) = @_;
+            my $uri = $headers->header('X-EndPoint-URL');
+            if ($uri) {
+                $request->uri($uri);
+                $headers->remove_header('X-EndPoint-URL');
+            };
+        }
+    ),
+);
+
+$proxy->start;
