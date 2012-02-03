15342c5e6a9fcbe53fdd680bfa30b88296f72aab
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2012-02-03 14:07:26 +0100

    New WS to WS router proxy

---
+++ ws2ws.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+# WS to WS router proxy server
+
+
+use strict;
+use warnings;
+
+use App::http2http;
+
+
+sub usage {
+    die "Usage $0 endpoint_hostport default_hostport\n";
+};
+
+
+my $proxy = App::http2http->new;
+
+my $endpoint_hostport = $proxy->{argv}->[0] || usage;
+my $default_hostport = $proxy->{argv}->[1] || usage;
+
+$proxy->{filter} = sub {
+    my ($self, $headers, $request) = @_;
+    my $endpoint = $headers->header('X-EndPoint-URL');
+    if ($endpoint) {
+        $request->uri->host_port($endpoint_hostport);
+    }
+    else {
+        $request->uri->host_port($default_hostport);
+    };
+};
+
+$proxy->start;
